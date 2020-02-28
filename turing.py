26d0620f2c1002e492e6bf2bf7d8687089749990
Osinowo Michael <36933062+micaris@users.noreply.github.com>
2020-02-28 01:42:41 +0000

    Delete turing.py

--- turing.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.stats import kurtosis, skew, pearsonr
-import seaborn as sns
-
-df = pd.read_csv('datasets/bank.csv')
-print(df.head())
